29b22b01c86967b0cdc2b7ba981e4250554bf046
Melroy Rodrigues <111352659+melroy-bsk@users.noreply.github.com>
2024-12-22 08:23:23 +0300

    Update create-dbs.sh

--- docker-compose/postgres/create-dbs.sh
+++ docker-compose/postgres/create-dbs.sh
@@ -4,12 +4,12 @@ set -x -o errexit -o errtrace -o nounset -o pipefail
 
 # setup extensions on template1 so future databases
 # all get the same extensions
-psql -v ON_ERROR_STOP=1 --username canvas -d template1 <<SQL
+psql -v ON_ERROR_STOP=1 --username postgres -d template1 <<SQL
   CREATE EXTENSION IF NOT EXISTS pg_trgm SCHEMA public;
 SQL
 
 for environment in production; do
-  psql -v ON_ERROR_STOP=1 --username canvas <<SQL
+  psql -v ON_ERROR_STOP=1 --username postgres <<SQL
     CREATE DATABASE canvas_${environment};
 SQL
 done
