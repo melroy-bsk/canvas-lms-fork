67578557aba489de88c72fcaf849830bede865a4
Melroy Rodrigues <111352659+melroy-bsk@users.noreply.github.com>
2024-12-22 13:42:24 +0300

    trying out something

--- docker-compose/postgres/create-dbs.sh
+++ docker-compose/postgres/create-dbs.sh
@@ -8,8 +8,18 @@ psql -v ON_ERROR_STOP=1 --username postgres -d template1 <<SQL
   CREATE EXTENSION IF NOT EXISTS pg_trgm SCHEMA public;
 SQL
 
-for environment in production; do
+# Create the canvas role with a password
+psql -v ON_ERROR_STOP=1 --username postgres <<SQL
+  CREATE ROLE canvas WITH LOGIN PASSWORD 'your_password';
+SQL
+
+for environment in test development production; do
   psql -v ON_ERROR_STOP=1 --username postgres <<SQL
     CREATE DATABASE canvas_${environment};
 SQL
 done
+
+# Grant privileges to the canvas_production database
+psql -v ON_ERROR_STOP=1 --username postgres <<SQL
+  GRANT ALL PRIVILEGES ON DATABASE canvas_production TO canvas;
+SQL
